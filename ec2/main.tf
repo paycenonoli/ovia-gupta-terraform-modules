53fb5e20282516acf5e9c17d7f7dc727b930a45d
DevopsWorking <159310985+DevopsWorking@users.noreply.github.com>
2024-03-03 07:38:38 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,3 @@ resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
  }
-
-output "public_ip" {
-  value = aws_instance.ec2_instance.public_ip
-}
